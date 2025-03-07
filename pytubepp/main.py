fb021fb7168a9f13b3fc640a3ba4c88cd3f2d06f
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2025-03-07 10:06:29 +0530

    (refactor): removed defaulting to 'WEB' client

--- pytubepp/main.py
+++ pytubepp/main.py
@@ -51,7 +51,7 @@ class YouTubeDownloader:
         
         if is_valid_url(link):
             link = is_valid_url(link).group(1)
-            self.video = YouTube(link, 'WEB', on_progress_callback=progress)
+            self.video = YouTube(link, on_progress_callback=progress)
             self.author = self.video.author
             self.title = re.sub(r'[\\/*?:"<>|]', '_', self.author + ' - ' + self.video.title)
             self.thumbnail = self.video.thumbnail_url
